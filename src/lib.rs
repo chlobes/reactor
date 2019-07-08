ef2e78dfc4be4bef90467caee2dceb4d6d7481ba
Your Name <you@example.com>
2019-07-08 16:06:45 +1000

    add water tank

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,7 @@ pub struct Reactor {
 	fuel: f32,
 	neutrons: f32,
 	heat: f32,
+	water: f32,
 	fuel_valve: bool,
 	vent: bool,
 }
@@ -27,17 +28,21 @@ impl Reactor {
 	pub fn new() -> Self {
 		utils::set_panic_hook();
 		Self {
-			fuel: 1.01,
-			neutrons: 1.0,
+			fuel: 0.7,
+			neutrons: 0.0,
 			heat: 0.0,
+			water: 0.0,
 			fuel_valve: true,
 			vent: false,
 		}
 	}
 	
-	pub fn tick(&mut self, delta: f32) {
+	pub fn tick(&mut self, delta: f32, water_tank: &mut WaterTank) {
 		if self.heat >= 3.0 || self.neutrons >= 3.0 || self.fuel >= 2.5 {
 			self.fuel_valve = false;
+		}
+		if self.heat >= 3.0 {
+			water_tank.unlocked = true;
 		}
 		let reaction_speed = (REACTION_RATE * self.fuel * self.neutrons - 1e-4).max(0.0);
 		let heat_exchange_rate = FUEL_NEUTRON_RATIO * REACTION_RATE * self.neutrons;
@@ -48,6 +53,15 @@ impl Reactor {
 		if self.neutrons >= 3.0 {
 			self.heat += NEUTRON_HEAT_RATIO * (self.neutrons - 3.0);
 			self.neutrons = 3.0;
+		}
+		if water_tank.unlocked {
+			let amount = delta * water_tank.valve * water_tank.water * (2.5 - self.water); //from water tank
+			self.heat = ((10.0 + self.water) * self.heat + amount * water_tank.heat) / (10.0 + self.water + amount);
+			self.water += amount;
+			water_tank.water -= amount;
+			let amount = delta * self.water * 0.1 * (25.0 - water_tank.water); //to water tank
+			self.water -= amount;
+			water_tank.add_water(amount, self.heat);
 		}
 		self.heat = self.heat.max(0.0);
 	}
@@ -75,6 +89,10 @@ impl Reactor {
 		self.heat
 	}
 	
+	pub fn water(&self) -> f32 {
+		self.water
+	}
+	
 	pub fn fuel_valve(&self) -> bool {
 		self.fuel_valve
 	}
@@ -85,5 +103,58 @@ impl Reactor {
 	
 	pub fn vent(&mut self, v: bool) {
 		self.vent = v;
+	}
+}
+
+#[wasm_bindgen]
+pub struct WaterTank {
+	unlocked: bool,
+	water: f32,
+	heat: f32,
+	valve: f32,
+}
+
+impl WaterTank {
+	fn add_water(&mut self, amount: f32, heat: f32) {
+		self.heat = ((self.water + 1.0) * self.heat + amount * heat) / (self.water + 1.0 + amount);
+		self.water += amount;
+	}
+}
+
+#[wasm_bindgen]
+impl WaterTank {
+	pub fn new() -> Self {
+		Self {
+			unlocked: false,
+			water: 20.0,
+			heat: 0.0,
+			valve: 0.01,
+		}
+	}
+	
+	pub fn tick(&mut self, delta: f32) {
+		if self.unlocked {
+			let amount = (delta * 0.005).min(25.0 - self.water);
+			self.add_water(amount, 0.0);
+			self.water -= delta * 0.01 * self.water.sqrt() * (self.heat.exp() - 1.0); //evaporation
+			self.heat -= delta * 0.00001 * (self.water.sqrt() + 1.0); //heat dissipation
+			self.heat = self.heat.max(0.0);
+		}
+	}
+	
+	pub fn water(&self) -> f32 {
+		self.water
+	}
+	
+	pub fn heat(&self) -> f32 {
+		self.heat
+	}
+	
+	pub fn render(&self) -> bool {
+		self.unlocked
+	}
+	
+	pub fn set_valve(&mut self, v: f32) {
+		self.valve = v;
 	}
 }
--- www/index.js
+++ www/index.js
@@ -1,13 +1,17 @@
-import { Reactor } from "reactor";
+import { Reactor, WaterTank } from "reactor";
+import {$, jQuery } from "jquery";
+//import roundSlider from "./roundslider.js";
 
 const BLACK = "#000000";
 const WHITE = "#FFFFFF";
 const DARK_GREEN = "#0C3D10";
 const DARK_GREY = "#828282";
 const GREY = "#C0C0C0";
-const LIGHT_GREY = "#E0E0E0";
+const LIGHT_GREY = "#D0D0D0";
+const GLASS = "#C9DBDC";
 const DULL_RED = "#954040";
 const RED = "#FF0000";
+const BLUE = "#0080FF";
 
 function blend(a, b, ratio) {
 	a = [parseInt(a[1] + a[2], 16), parseInt(a[3] + a[4], 16), parseInt(a[5] + a[6], 16)];
@@ -26,6 +30,22 @@ function int_to_hex(num) {
 		hex = '0' + hex;
 	return hex;
 }
+
+const water_tank = WaterTank.new();
+const water_tank_canvas = document.getElementById("water-tank-canvas");
+water_tank_canvas.width = 300;
+water_tank_canvas.height = 155;
+const water_tank_ctx = water_tank_canvas.getContext('2d');
+var water_tank_unlocked = false;
+
+/*const water_tank_slider = roundSlider.CreateRoundSlider({
+	radius: 80,
+	circleShape: "quarter-top-right",
+	sliderType: "min-range",
+	showTooltip: false,
+	value: 100
+});*/ //TODO: figure out how javascript syntax works
+const water_tank_slider	= document.getElementById("water-valve-slider");
 
 const reactor = Reactor.new();
 const reactor_canvas = document.getElementById("reactor-canvas");
@@ -55,39 +75,72 @@ vent_button.addEventListener("click", event => {
 	reactor.vent(venting);
 });
 
-const drawReactor = () => {
-	var heat = Math.min(Math.log(1.1 * reactor.heat()+1.0), 2.0);
-	reactor_ctx.fillStyle = heat >= 1.0
-		? blend(GREY, RED, 0.7 * (heat - 1.0))
-		: blend(DARK_GREY, GREY, heat);
-	reactor_ctx.fillRect(0, 0, reactor_canvas.width, reactor_canvas.height);
-	reactor_ctx.fillStyle = BLACK;
-	reactor_ctx.fillRect(5, 5, 20, 150);
-	reactor_ctx.fillRect(30, 35, 20, 120);
-	//reactor_ctx.fillRect(55, 35, 20, 120);
-	reactor_ctx.fillStyle = DULL_RED;
-	//reactor_ctx.fillRect(5, 5, 20, 30);
-	reactor_ctx.fillRect(30, 5, 20, 30);
-	//reactor_ctx.fillRect(55, 5, 20, 30);
-	reactor_ctx.fillStyle = LIGHT_GREY;
-	reactor_ctx.fillRect(5, 155, 20, -60*reactor.fuel());
-	reactor_ctx.fillRect(30, 155, 20, -60*Math.min(reactor.neutrons(), 2.0));
-	if (reactor.neutrons() > 2) {
-		reactor_ctx.fillStyle = RED;
-		reactor_ctx.fillRect(30, 35, 20, -30*Math.min(reactor.neutrons() - 2.0, 1.0));
+const drawWaterTank = () => {
+	if (water_tank.render()) {
+		if (!water_tank_unlocked) {
+			water_tank_unlocked = true;
+			water_tank_slider.oninput = function() {
+				water_tank.set_valve(this.value / 1000.0);
+			};
+			water_tank_slider.style.visibility = "visible";
+			water_tank_slider.value = 10;
+		}
+		var heat = Math.min(water_tank.heat(), 2.0);
+		water_tank_ctx.fillStyle = heat >= 1.0
+			? blend(LIGHT_GREY, RED, 0.8 * (heat - 1.0))
+			: blend(GREY, LIGHT_GREY, heat);
+		water_tank_ctx.fillRect(0, 0, 300, 155);
+		water_tank_ctx.fillStyle = blend(GLASS, RED, 0.6 * Math.max(heat - 1.0, 0.0));
+		water_tank_ctx.fillRect(5, 0, 290, 150);
+		water_tank_ctx.fillStyle = BLUE;
+		water_tank_ctx.fillRect(5, 150, 290, -6*water_tank.water());
 	}
-	reactor_ctx.strokeStyle = DARK_GREEN;
-	reactor_ctx.beginPath();
-	reactor_ctx.moveTo(5, 95);
-	reactor_ctx.lineTo(50, 95);
-	reactor_ctx.stroke();
+};
+
+const drawReactor = () => {
+	if (reactor.render()) {
+		var heat = Math.min(Math.log(1.1 * reactor.heat()+1.0), 2.0);
+		reactor_ctx.fillStyle = heat >= 1.0
+			? blend(GREY, RED, 0.7 * (heat - 1.0))
+			: blend(DARK_GREY, GREY, heat);
+		reactor_ctx.fillRect(0, 0, reactor_canvas.width, reactor_canvas.height);
+		reactor_ctx.fillStyle = BLACK;
+		reactor_ctx.fillRect(30, 5, 20, 150);
+		reactor_ctx.fillRect(55, 35, 20, 120);
+		reactor_ctx.fillStyle = DULL_RED;
+		reactor_ctx.fillRect(55, 5, 20, 30);
+		reactor_ctx.fillStyle = LIGHT_GREY;
+		reactor_ctx.fillRect(30, 155, 20, -60*reactor.fuel());
+		reactor_ctx.fillRect(55, 155, 20, -60*Math.min(reactor.neutrons(), 2.0));
+		if (reactor.neutrons() > 2) {
+			reactor_ctx.fillStyle = RED;
+			reactor_ctx.fillRect(55, 35, 20, -30*Math.min(reactor.neutrons() - 2.0, 1.0));
+		}
+		reactor_ctx.strokeStyle = DARK_GREEN;
+		reactor_ctx.beginPath();
+		reactor_ctx.moveTo(30, 95);
+		reactor_ctx.lineTo(75, 95);
+		reactor_ctx.stroke();
+		if (water_tank.render()) {
+			reactor_ctx.fillStyle = BLACK;
+			reactor_ctx.fillRect(5, 5, 20, 150);
+			reactor_ctx.fillRect(5, 5, 20, 30);
+			reactor_ctx.fillRect(5, 5, 20, 30);
+			reactor_ctx.fillStyle = BLUE;
+			reactor_ctx.fillRect(5, 155, 20, -60*reactor.water());
+		}
+	}
 };
 
 const renderLoop = () => {
 	var delta = 1/60;
-	reactor.tick(delta);
-	fuel_valve.textContent = reactor.fuel_valve() ? "💧" : "○";
+	
+	reactor.tick(delta, water_tank);
+	fuel_valve.textContent = reactor.fuel_valve() ? "placeholder💧" : "placeholder○";
 	drawReactor();
+	
+	water_tank.tick(delta);
+	drawWaterTank();
 	
 	requestAnimationFrame(renderLoop);
 };
